face29e6005cc1d17e36dc6d7b5050f15c47d329
André Company Gianesi <andrecompanygianesi@Andres-Air.lan>
2024-11-03 01:59:22 -0400

    created role table

--- schema.sql
+++ schema.sql
@@ -9,3 +9,10 @@ CREATE TABLE department (
     id SERIAL PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
 );
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL REFERENCES department(id)
+);
