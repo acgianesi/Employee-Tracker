ceb7b6fc92a7c2e5f8bbf4e457b6677e3701e6b2
André Company Gianesi <andrecompanygianesi@Andres-Air.lan>
2024-11-03 02:48:21 -0500

    created function addEmployee and function updateEmployeeRole

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const db = require ('./db');
+const db = require('./db');
 
 function displayMainMenu() {
     inquirer.prompt({
@@ -17,30 +17,30 @@ function displayMainMenu() {
             'Exit'
         ]
     })
-    .then(answer => {
-        if (answer.action === 'View all departments') {
-            viewDepartments();
-        } else if (answer.action === 'View all roles') {
-            viewRoles();
-        } else if (answer.action === 'View all employees') {
-            viewEmployees();
-        } else if (answer.action === 'Add a department') {
-            addDepartment();
-        } else if (answer.action === 'Add a role') {
-            addRole();
-        } else if (answer.action === 'Add an employee') {
-            addEmployee();
-        } else if (answer.action === 'Update an employee role') {
-            updateEmployeeRole();
-        } else if (answer.action === 'Exit') {
-            db.end();
-        }
-    });
+        .then(answer => {
+            if (answer.action === 'View all departments') {
+                viewDepartments();
+            } else if (answer.action === 'View all roles') {
+                viewRoles();
+            } else if (answer.action === 'View all employees') {
+                viewEmployees();
+            } else if (answer.action === 'Add a department') {
+                addDepartment();
+            } else if (answer.action === 'Add a role') {
+                addRole();
+            } else if (answer.action === 'Add an employee') {
+                addEmployee();
+            } else if (answer.action === 'Update an employee role') {
+                updateEmployeeRole();
+            } else if (answer.action === 'Exit') {
+                db.end();
+            }
+        });
 }
 
 function viewDepartments() {
     console.log("Viewing all departments...");
-    displayMainMenu(); 
+    displayMainMenu();
 }
 
 function viewRoles() {
@@ -59,10 +59,10 @@ function addDepartment() {
         name: 'departmentName',
         message: 'Enter the name of the department:'
     })
-    .then(answer => {
-        console.log(`Adding department: ${answer.departmentName}`);
-        displayMainMenu();
-    });
+        .then(answer => {
+            console.log(`Adding department: ${answer.departmentName}`);
+            displayMainMenu();
+        });
 }
 
 function addRole() {
@@ -83,8 +83,42 @@ function addRole() {
             message: 'Enter the department ID for this role:'
         }
     ])
+        .then(answer => {
+            console.log(`Adding role: ${answer.roleTitle}`);
+            displayMainMenu();
+        });
+}
+
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'firstName',
+            message: 'Enter the first name of the employee:'
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'Enter the last name of the employee:'
+        },
+        {
+            type: 'input',
+            name: 'roleId',
+            message: 'Enter the role ID for this employee:'
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: 'Enter the manager ID for this employee (leave blank if none):'
+        }
+    ])
     .then(answer => {
-        console.log(`Adding role: ${answer.roleTitle}`);
+        console.log(`Adding employee: ${answer.firstName} ${answer.lastName}`);
         displayMainMenu();
     });
+}
+
+function updateEmployeeRole() {
+    console.log("Updating employee role...");
+    displayMainMenu();
 }
