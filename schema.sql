10926ac791cc10bbccc5f023fe920f64fbd4f653
André Company Gianesi <andrecompanygianesi@Andres-Air.lan>
2024-11-03 01:25:04 -0500

    fixed issue

--- schema.sql
+++ schema.sql
@@ -1,10 +1,3 @@
-DROP DATABASE IF EXISTS employee_tracker;
-
-CREATE DATABASE employee_tracker;
-
-\c employee_tracker;
-
-
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
