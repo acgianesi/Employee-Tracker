6cc3beac3321de8ad98e6079e33d080d6f077b2e
André Company Gianesi <andrecompanygianesi@Andres-Air.lan>
2024-11-03 01:00:39 -0500

    created employee table

--- schema.sql
+++ schema.sql
@@ -16,3 +16,11 @@ CREATE TABLE role (
     salary DECIMAL NOT NULL,
     department_id INTEGER NOT NULL REFERENCES department(id)
 );
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL REFERENCES role(id),
+    manager_id INTEGER REFERENCES employee(id)
+);
