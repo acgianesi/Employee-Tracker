8ae7ed7987ee6c60b921a368a5bd9c10dd82b6ec
André Company Gianesi <andrecompanygianesi@Andres-Air.lan>
2024-11-03 01:58:07 -0500

    added index.js file and created function called displayMainMenu

---
+++ index.js
@@ -0,0 +1,39 @@
+const inquirer = require('inquirer');
+const db = require ('./db');
+
+function displayMainMenu() {
+    inquirer.prompt({
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Exit'
+        ]
+    })
+    .then(answer => {
+        if (answer.action === 'View all departments') {
+            viewDepartments();
+        } else if (answer.action === 'View all roles') {
+            viewRoles();
+        } else if (answer.action === 'View all employees') {
+            viewEmployees();
+        } else if (answer.action === 'Add a department') {
+            addDepartment();
+        } else if (answer.action === 'Add a role') {
+            addRole();
+        } else if (answer.action === 'Add an employee') {
+            addEmployee();
+        } else if (answer.action === 'Update an employee role') {
+            updateEmployeeRole();
+        } else if (answer.action === 'Exit') {
+            db.end();
+        }
+    });
+}
